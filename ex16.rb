f95176acd913ea8fd869f26cf33af28800558d72
AidiRi <rivera.aidi@gmail.com>
2023-02-09 16:55:03 -0800

    Ex16: simplified code; reducing write() methods, removig truncate()

--- ex16.rb
+++ ex16.rb
@@ -8,15 +8,11 @@ puts "If you do want that, then hit RETURN."
 $stdin.gets
 
 puts "Opening the file..."
-# - Opens file with 'w' as mode option that only allows writing, not reading
-# target = open(filename, 'w')
-
 # - open() method with mode option 'w+' allows reading, still with initial truncate
 target = open(filename, 'w+')
 
 puts "Truncating the file. Goodbye!"
-# - Deletes contents of file, to be at most 0 bytes long
-target.truncate(0)
+# - Initial truncate after open() with mode option 'w' means target.truncate(0) is redundant
 
 puts "Now I'm going to ask you for three lines."
 
@@ -29,14 +25,9 @@ line3 = $stdin.gets.chomp
 
 puts "I'm going to write these to the file."
 
-# - Writes line1 to the target file
-target.write(line1)
-# - Writes a line feed, moves one line foward
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write("#{line1}\n#{line2}\n#{line3}\n")
+
+$stdin.gets
 
 puts "Now lets read what we've written:"
 # - Method seek() will move the 'cursor' (position) to the integer given, the byte offset 
